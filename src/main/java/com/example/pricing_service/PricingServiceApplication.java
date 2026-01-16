f015712a696426a9d537ee3de358a9b62bb0e2f4
201912025 <michi2012@naver.com>
2026-01-16 13:12:08 +0900

    feat: 스레드 전환 구간에서 확실한 전파 위해 Hooks 설정 명시적으로 추가

--- src/main/java/com/example/pricing_service/PricingServiceApplication.java
+++ src/main/java/com/example/pricing_service/PricingServiceApplication.java
@@ -3,12 +3,15 @@ package com.example.pricing_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PricingServiceApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
+
 		SpringApplication.run(PricingServiceApplication.class, args);
 	}
 
